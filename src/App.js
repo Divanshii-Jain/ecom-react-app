f7650b97007a98ad8da7cf9695a4f5cfc30ec2ea
Divanshii-Jain <work.divanshij@gmail.com>
2024-06-15 13:28:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import Home from './Components/Pages/Home';
-import About from './Components/Pages/About';
-import Layout from './Components/Layouts/Layout';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
+import Layout from "./Components/Layouts/Layout";
 // import SignUp from './Components/Pages/SignUp';
 // import Header from './Components/Pages/Header';
-import LearnHooks from './Components/Modules/LearnHooks';
-import Products from './Components/Modules/Products';
-import Add from './Components/Pages/Admin/Category/Add';
-
+import LearnHooks from "./Components/Modules/LearnHooks";
+import Products from "./Components/Modules/Products";
+import Add from "./Components/Pages/Admin/Category/Add";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           {/* <Route path='/header' element={<Header/>}/> */}
-          {/* <Route index element={<Home />} /> */}
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           {/* <Route path='/signup' element={<SignUp />} /> */}
-          <Route path='/learnhooks' element={<LearnHooks/>} />
+          <Route path="/learnhooks" element={<LearnHooks />} />
         </Route>
       </Routes>
-      <Products/>
+      <Products />
       <Add />
     </BrowserRouter>
   );
--- src/Components/Modules/Products.jsx
+++ src/Components/Modules/Products.jsx
@@ -1,41 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import {Container, Row } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
 
 const Products = () => {
-    const [items, setItems] = useState ([]);
-    useEffect(() => {
-        fetch('http://localhost:4001/api/category') //make sure the address is right
-        .then((res) => res.json())
-        .then((data) => setItems(data))
-        .catch((error) => console.log(error));
-    }, [])
-
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4001/api/category") //make sure the address is right
+      .then((res) => res.json())
+      .then((data) => setItems(data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <Container>
-        <Row>
+      <Row>
         <table>
-            <thead>
+          <thead>
             <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Photo</th>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Photo</th>
             </tr>
-            </thead>
-
-        <tbody>
-        {items.map((item) =>(
-            <tr key={item._id}>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item._id}>
                 <td>{item.title}</td>
                 <td>{item.description}</td>
-                <td><img alt="" src= {item.thumbnail} height="100" /></td>
-            </tr>
-        ))}
-        </tbody>
+                <td>
+                  <img alt="" src={item.thumbnail} height="100" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        </Row>
+      </Row>
     </Container>
   );
-}
+};
 
-export default Products
+export default Products;
